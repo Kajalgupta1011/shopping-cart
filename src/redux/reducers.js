d463a7e3cd12ec4e83a70b9a1d95c9c044c88d73
Kajal Gupta <84381877+Kajalgupta1011@users.noreply.github.com>
2023-11-09 14:53:32 +0530

    Update reducers.js

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -4,18 +4,21 @@ const initialState = {
   };
   // reducers.js
 export const cartReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case 'ADD_TO_CART':
-        return {
-          ...state,
-          cart: [...state.cart, action.product],
-        };
-      case 'REMOVE_FROM_CART':
-        return {
-          ...state,
-          cart: state.cart.filter((product) => product.id !== action.productId),
-        };
-      default:
-        return state;
-    };
-}
+  console.log('Action:', action);
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      console.log('Adding to cart:', action.product);
+      return {
+        ...state,
+        cart: [...state.cart, action.product],
+      };
+    case 'REMOVE_FROM_CART':
+      console.log('Removing from cart:', action.productId);
+      return {
+        ...state,
+        cart: state.cart.filter((product) => product.id !== action.productId),
+      };
+    default:
+      return state;
+  }
+};
